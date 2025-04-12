2b02768548dc2147af573c2ebe4db5a5b076d9b6
estelledenis <73923523+estelledenis@users.noreply.github.com>
2025-04-11 22:43:58 -0400

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -5,17 +5,18 @@ def insert_login_attempt(username, ip_address, status, event_time=None, db_path=
     c = conn.cursor()
     c.execute('''
         CREATE TABLE IF NOT EXISTS login_attempts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT,
-            ip_address TEXT,
-            status TEXT NOT NULL,
-            event_time TEXT
-        )
-    ''')
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    ip_address TEXT,
+    status TEXT NOT NULL,
+    event_time TEXT
+);
+
     c.execute('''
-        INSERT INTO login_attempts (username, ip_address, status, event_time)
-        VALUES (?, ?, ?, ?)
-    ''', (username, ip_address, status, event_time))
+    INSERT INTO login_attempts (username, ip_address, status, event_time)
+    VALUES (?, ?, ? ,?)
+''', (username, ip_address, status, event_time))
+
     print(f"Inserting: user={username}, ip={ip_address}, status={status}, time={event_time} into {db_path}")
     conn.commit()
     conn.close()
@@ -27,11 +28,12 @@ def get_recent_logins(limit=50, db_path="system_monitor.db"):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute('''
-        SELECT username, ip_address, status, timestamp
-        FROM login_attempts
-        ORDER BY id DESC
-        LIMIT ?
-    ''', (limit,))
+    SELECT username, ip_address, status, event_time
+    FROM login_attempts
+    ORDER BY id DESC
+    LIMIT ?
+''', (limit,))
+
     rows = c.fetchall()
     conn.close()
     return rows
