24f24d3e6defe2e97ce3f88bb64b915e502399e5
estelledenis <73923523+estelledenis@users.noreply.github.com>
2025-04-12 22:57:08 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -145,7 +145,7 @@ def create_button(text, command, color):
 
 firewall_script = os.path.join(BASE_DIR, "firewall_creation", "firewall_rule_gen_windows.py" if IS_WINDOWS else "firewall_rule_gen.py")
 log_script = os.path.join(BASE_DIR, "log_monitoring", "windows_log_parsing.py" if IS_WINDOWS else "log_parsing.py")
-vuln_script = os.path.join(BASE_DIR, "vulnerability_scan", "nmap_scan.py")
+vuln_script = os.path.join(BASE_DIR, "vulnerability_scan", "nmap_scan_windows.py" if IS_WINDOWS else "nmap_scan.py")
 
 button_frame = tk.Frame(root, bg="#2C3E50")
 button_frame.pack(pady=20, fill="x", expand=True)
